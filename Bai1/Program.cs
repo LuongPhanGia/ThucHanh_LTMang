8c90327095b5b7e2f0092e093a193e3e323b425a
luongluong <phangialuong01@gmail.com>
2018-03-09 21:29:41 +0700

    luongluong

---
+++ Bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry iPHostEntry = Dns.Resolve(hostName);
+            Console.WriteLine(iPHostEntry.HostName);
+            foreach(IPAddress s in iPHostEntry.AddressList)
+            {
+                Console.WriteLine(s);
+            }
+
+       
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien: ");
+            string hostName = Console.ReadLine();
+            IPHostEntry iPHostEntry = Dns.Resolve(hostName);
+            Console.WriteLine(iPHostEntry.HostName);
+            foreach (IPAddress s in iPHostEntry.AddressList)
+            {
+                Console.WriteLine(s);
+            }
+
+
+        }
+    }
+}
